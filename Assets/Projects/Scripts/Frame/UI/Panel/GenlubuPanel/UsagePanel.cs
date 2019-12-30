6bbb31377f9d94a2652cfa808b852c1b7289b1b7
oubaoyao <673411498@qq.com>
2019-12-30 11:49:32 +0800

    更路簿使用说明Panel添加了视频

--- Assets/Projects/Scripts/Frame/UI/Panel/GenlubuPanel/UsagePanel.cs
+++ Assets/Projects/Scripts/Frame/UI/Panel/GenlubuPanel/UsagePanel.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 using MTFrame;
 using MTFrame.MTEvent;
 using UnityEngine.UI;
+using RenderHeads.Media.AVProVideo;
 
 public class UsagePanel : BasePanel
 {
     public Button BackButton;
 
+    public MediaPlayer mediaPlayer;
+
     public override void InitFind()
     {
         base.InitFind();
         BackButton = FindTool.FindChildComponent<Button>(transform, "backButton");
+
+        mediaPlayer = FindTool.FindChildComponent<MediaPlayer>(transform, "VideoBG/Video Player/Video Player");
     }
 
     public override void InitEvent()
@@ -23,4 +28,15 @@ public class UsagePanel : BasePanel
         });
     }
 
+    public override void Open()
+    {
+        base.Open();
+        mediaPlayer.OpenVideoFromFile(MediaPlayer.FileLocation.RelativeToStreamingAssetsFolder, "AVProVideoSamples/DA-5-更路簿查询.mp4");
+    }
+
+    public override void Hide()
+    {
+        base.Hide();
+        mediaPlayer.Control.Stop();
+    }
 }
